3b33f9322e67be51b50cbe9639ff4e8facc45d37
leron99 <80694096+leron99@users.noreply.github.com>
2023-03-27 17:08:18 +0800

    edit playermovement on server

--- VI Engine/APPLICATION/source/Scripts/Old/BunnyChef_PlayerMovement.cpp
+++ VI Engine/APPLICATION/source/Scripts/Old/BunnyChef_PlayerMovement.cpp
@@ -87,76 +87,59 @@ void BunnyChef_PlayerMovement::Update(const Entity& _e) {
 	INGAMESTATEMANAGER = VI::iEntity::GetEntity("INGAMESTATEMANAGER", "");
 	NetworkSerializationManager* manager = NetworkSerializationManager::GetInstance();
 	ServerManager* server = ServerManager::GetInstance();
-#ifdef _SERVER
-	// Update character directions
-	float movementSpeed{ 40.f };
-
-	if (server->mP1Input[0])	{
-		VI::iPhysics::ApplyImpulse(BunnyPlayer, { 0, movementSpeed }, 0.0f);
-		std::cout << "mP1InputW\n";
-	}
-	else if (server->mP1Input[1]) {
-		VI::iPhysics::ApplyImpulse(BunnyPlayer, { 0, -movementSpeed }, 0.0f);
-		std::cout << "mP1InputS\n";
-	}
-	else if (server->mP1Input[2]) {
-		VI::iPhysics::ApplyImpulse(BunnyPlayer, { -movementSpeed, 0 }, 0.0f);
-		std::cout << "mP1InputA\n";
-	}
-	else if (server->mP1Input[3]) {
-		VI::iPhysics::ApplyImpulse(BunnyPlayer, { movementSpeed, 0 }, 0.0f);
-		std::cout << "mP1InputD\n";
-	}
-
-	if (server->mP2Input[0]) {
-		VI::iPhysics::ApplyImpulse(ChefPlayer, { 0, movementSpeed }, 0.0f);
-		std::cout << "mP2InputW\n";
-	} else if (server->mP2Input[1]) {
-		VI::iPhysics::ApplyImpulse(ChefPlayer, { 0, -movementSpeed }, 0.0f);
-		std::cout << "mP2InputS\n";
-	} else if (server->mP2Input[2]) {
-		VI::iPhysics::ApplyImpulse(ChefPlayer, { -movementSpeed, 0 }, 0.0f);
-		std::cout << "mP2InputA\n";
-	} else if (server->mP2Input[3]) {
-		VI::iPhysics::ApplyImpulse(ChefPlayer, { movementSpeed, 0 }, 0.0f);
-		std::cout << "mP2InputD\n";
-	}
-
-	//std::cout << "mP1Input: " << manager->mP1Input << '\n';
-	//std::cout << "mP2Input: " << manager->mP2Input << '\n';
-
-
-	// Update bunny movement
-	//MoveCharacter(BunnyPlayer, sPLAYERDIRECTION);
-	//MoveCharacter(ChefPlayer, sCHEFDIRECTION);
-#endif
-
-	//std::cout << "state: " << INGAMESTATEMANAGER.GetComponent<Bunny_InGameStateComponent>().bigs << std::endl;
 	if (INGAMESTATEMANAGER.GetComponent<Bunny_InGameStateComponent>().bigs == BUNNY_INGAME) {
+#ifdef _SERVER
+		// Update character directions
+		/*float movementSpeed{ 40.f };
 
+		if (server->mP1Input[0]) {
+			VI::iPhysics::ApplyImpulse(BunnyPlayer, { 0, movementSpeed }, 0.0f);
+			std::cout << "mP1InputW\n";
+		}
+		else if (server->mP1Input[1]) {
+			VI::iPhysics::ApplyImpulse(BunnyPlayer, { 0, -movementSpeed }, 0.0f);
+			std::cout << "mP1InputS\n";
+		}
+		else if (server->mP1Input[2]) {
+			VI::iPhysics::ApplyImpulse(BunnyPlayer, { -movementSpeed, 0 }, 0.0f);
+			std::cout << "mP1InputA\n";
+		}
+		else if (server->mP1Input[3]) {
+			VI::iPhysics::ApplyImpulse(BunnyPlayer, { movementSpeed, 0 }, 0.0f);
+			std::cout << "mP1InputD\n";
+		}
 
-#ifdef _CLIENT
+		if (server->mP2Input[0]) {
+			VI::iPhysics::ApplyImpulse(ChefPlayer, { 0, movementSpeed }, 0.0f);
+			std::cout << "mP2InputW\n";
+		}
+		else if (server->mP2Input[1]) {
+			VI::iPhysics::ApplyImpulse(ChefPlayer, { 0, -movementSpeed }, 0.0f);
+			std::cout << "mP2InputS\n";
+		}
+		else if (server->mP2Input[2]) {
+			VI::iPhysics::ApplyImpulse(ChefPlayer, { -movementSpeed, 0 }, 0.0f);
+			std::cout << "mP2InputA\n";
+		}
+		else if (server->mP2Input[3]) {
+			VI::iPhysics::ApplyImpulse(ChefPlayer, { movementSpeed, 0 }, 0.0f);
+			std::cout << "mP2InputD\n";
+		}*/
 
-		if (!BunnyPlayer.HasComponent<Bunny>())
-			BunnyPlayer.AddComponent(Bunny{});
-
-
-		//Move Up
-		if (NetworkSerializationManager::GetInstance()->mPlayerID == 1)
 		{
-			if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::W))
-			{
-				if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP)
-				{
-					sPLAYERDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP;
-					BunnyPlayer.GetComponent<Physics2D>().acceleration = 0;
-					BunnyPlayer.GetComponent<Physics2D>().velocity.x = 0;
-					BunnyPlayer.GetComponent<Physics2D>().velocity.y = 0;
-				}
+			if (server->mP1Input[0]))
+		{
+		if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP)
+		{
+			sPLAYERDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP;
+			BunnyPlayer.GetComponent<Physics2D>().acceleration = 0;
+			BunnyPlayer.GetComponent<Physics2D>().velocity.x = 0;
+			BunnyPlayer.GetComponent<Physics2D>().velocity.y = 0;
+		}
 			}
 
 			//Move Down
-			if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::S))
+			if (server->mP1Input[1])
 			{
 				if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_DOWN)
 				{
@@ -168,7 +151,7 @@ void BunnyChef_PlayerMovement::Update(const Entity& _e) {
 			}
 
 			//Move Left
-			if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::A))
+			if (server->mP1Input[2])
 			{
 				if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_LEFT)
 				{
@@ -183,7 +166,7 @@ void BunnyChef_PlayerMovement::Update(const Entity& _e) {
 			}
 
 			//Move Right
-			if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::D))
+			if (server->mP1Input[3])
 			{
 				if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_RIGHT)
 				{
@@ -214,6 +197,167 @@ void BunnyChef_PlayerMovement::Update(const Entity& _e) {
 		default:
 			break;
 		}
+	}
+
+
+
+	//Mac Chef
+
+	{
+		if (server->mP2Input[0])
+		{
+			if (sCHEFDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP)
+			{
+				sCHEFDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP;
+				ChefPlayer.GetComponent<Physics2D>().acceleration = 0;
+				ChefPlayer.GetComponent<Physics2D>().velocity.x = 0;
+				ChefPlayer.GetComponent<Physics2D>().velocity.y = 0;
+			}
+		}
+		if (server->mP2Input[1])
+		{
+			if (sCHEFDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_DOWN)
+			{
+				sCHEFDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_DOWN;
+				ChefPlayer.GetComponent<Physics2D>().acceleration = 0;
+				ChefPlayer.GetComponent<Physics2D>().velocity.x = 0;
+				ChefPlayer.GetComponent<Physics2D>().velocity.y = 0;
+			}
+		}
+		if (server->mP2Input[2])
+		{
+			if (sCHEFDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_LEFT)
+			{
+				if (ChefPlayer.GetComponent<Transform>().scale.x > 0.0f)
+					ChefPlayer.GetComponent<Transform>().scale.x = -ChefPlayer.GetComponent<Transform>().scale.x;
+				sCHEFDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_LEFT;
+				ChefPlayer.GetComponent<Physics2D>().acceleration = 0;
+				ChefPlayer.GetComponent<Physics2D>().velocity.x = 0;
+				ChefPlayer.GetComponent<Physics2D>().velocity.y = 0;
+
+			}
+		}
+		if (server->mP2Input[3])
+		{
+			if (sCHEFDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_RIGHT)
+			{
+				if (ChefPlayer.GetComponent<Transform>().scale.x < 0.0f)
+					ChefPlayer.GetComponent<Transform>().scale.x = -ChefPlayer.GetComponent<Transform>().scale.x;
+				sCHEFDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_RIGHT;
+				ChefPlayer.GetComponent<Physics2D>().acceleration = 0;
+				ChefPlayer.GetComponent<Physics2D>().velocity.x = 0;
+				ChefPlayer.GetComponent<Physics2D>().velocity.y = 0;
+
+			}
+		}
+	}
+
+	switch (sCHEFDIRECTION)
+	{
+	case BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP:
+		VI::iPhysics::ApplyImpulse(ChefPlayer, { 0.0f,40.0f }, 0.0f);
+		break;
+	case BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_DOWN:
+		VI::iPhysics::ApplyImpulse(ChefPlayer, { 0.0f,-40.0f }, 0.0f);
+		break;
+	case BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_LEFT:
+		VI::iPhysics::ApplyImpulse(ChefPlayer, { -40.0f,0.0f }, 0.0f);
+		break;
+	case BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_RIGHT:
+		VI::iPhysics::ApplyImpulse(ChefPlayer, { 40.0f,0.0f }, 0.0f);
+		break;
+	default:
+		break;
+		//std::cout << "mP1Input: " << manager->mP1Input << '\n';
+		//std::cout << "mP2Input: " << manager->mP2Input << '\n';
+
+	}
+	// Update bunny movement
+	//MoveCharacter(BunnyPlayer, sPLAYERDIRECTION);
+	//MoveCharacter(ChefPlayer, sCHEFDIRECTION);
+#endif
+
+	//std::cout << "state: " << INGAMESTATEMANAGER.GetComponent<Bunny_InGameStateComponent>().bigs << std::endl;
+
+
+#ifdef _CLIENT
+
+	if (!BunnyPlayer.HasComponent<Bunny>())
+		BunnyPlayer.AddComponent(Bunny{});
+
+
+	//Move Up
+	if (NetworkSerializationManager::GetInstance()->mPlayerID == 1)
+	{
+		if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::W))
+		{
+			if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP)
+			{
+				sPLAYERDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP;
+				BunnyPlayer.GetComponent<Physics2D>().acceleration = 0;
+				BunnyPlayer.GetComponent<Physics2D>().velocity.x = 0;
+				BunnyPlayer.GetComponent<Physics2D>().velocity.y = 0;
+			}
+		}
+
+		//Move Down
+		if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::S))
+		{
+			if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_DOWN)
+			{
+				sPLAYERDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_DOWN;
+				BunnyPlayer.GetComponent<Physics2D>().acceleration = 0;
+				BunnyPlayer.GetComponent<Physics2D>().velocity.x = 0;
+				BunnyPlayer.GetComponent<Physics2D>().velocity.y = 0;
+			}
+		}
+
+		//Move Left
+		if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::A))
+		{
+			if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_LEFT)
+			{
+				if (BunnyPlayer.GetComponent<Transform>().scale.x > 0.0f)
+					BunnyPlayer.GetComponent<Transform>().scale.x = -BunnyPlayer.GetComponent<Transform>().scale.x;
+				sPLAYERDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_LEFT;
+				BunnyPlayer.GetComponent<Physics2D>().acceleration = 0;
+				BunnyPlayer.GetComponent<Physics2D>().velocity.x = 0;
+				BunnyPlayer.GetComponent<Physics2D>().velocity.y = 0;
+
+			}
+		}
+
+		//Move Right
+		if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::D))
+		{
+			if (sPLAYERDIRECTION != BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_RIGHT)
+			{
+				if (BunnyPlayer.GetComponent<Transform>().scale.x < 0.0f)
+					BunnyPlayer.GetComponent<Transform>().scale.x = -BunnyPlayer.GetComponent<Transform>().scale.x;
+				sPLAYERDIRECTION = BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_RIGHT;
+				BunnyPlayer.GetComponent<Physics2D>().acceleration = 0;
+				BunnyPlayer.GetComponent<Physics2D>().velocity.x = 0;
+				BunnyPlayer.GetComponent<Physics2D>().velocity.y = 0;
+
+			}
+		}
+	}
+	switch (sPLAYERDIRECTION)
+	{
+	case BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_UP:
+		VI::iPhysics::ApplyImpulse(BunnyPlayer, { 0.0f,40.0f }, 0.0f);
+		break;
+	case BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_DOWN:
+		VI::iPhysics::ApplyImpulse(BunnyPlayer, { 0.0f,-40.0f }, 0.0f);
+		break;
+	case BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_LEFT:
+		VI::iPhysics::ApplyImpulse(BunnyPlayer, { -40.0f,0.0f }, 0.0f);
+		break;
+	case BUNNY_PLAYER_DIRECTION::BUNNY_DIRECTION_RIGHT:
+		VI::iPhysics::ApplyImpulse(BunnyPlayer, { 40.0f,0.0f }, 0.0f);
+		break;
+	default:
+		break;
 	}
 
 
@@ -286,9 +430,10 @@ void BunnyChef_PlayerMovement::Update(const Entity& _e) {
 		break;
 	default:
 		break;
-#endif
 	}
-		//if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::SPACE)) { BunnyOrChef = !BunnyOrChef; }
+#endif
+	//if (VI::iInput::CheckKey(E_STATE::PRESS, E_KEY::SPACE)) { BunnyOrChef = !BunnyOrChef; }
+	}
 }
 
 void BunnyChef_PlayerMovement::FixedUpdate(const Entity& _e) {
