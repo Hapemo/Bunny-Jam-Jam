4fa2333f036ca794e7434210cd1405865569b4fb
Jie Hui <90439407+jiehui69@users.noreply.github.com>
2023-03-29 11:34:30 +0800

    updated EI yet to try'

--- VI Engine/APPLICATION/source/Scripts/ConnectionButtonControl.cpp
+++ VI Engine/APPLICATION/source/Scripts/ConnectionButtonControl.cpp
@@ -277,7 +277,7 @@ void ConnectionButtonControl::Update(Entity const& _e) {
         {
             if (!isConnected)
             {
-                ipstring_ = "192.168.136.176";
+                //ipstring_ = "192.168.136.176";
                 //ipstring_ = "192.168.50.172";
                 //ipstring_ = "192.168.50.16";
                 if (ClientManager::GetInstance()->clientInit(ipstring_, 5051, "gay"))
